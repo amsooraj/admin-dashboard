6ff45a82bda0b5aea7e2a1866c5a40e98d92bd3a
amsooraj <soorajkannan2000@gmail.com>
2023-04-24 12:20:51 +0530

    admin dashboard using angular material,highcharts

---
+++ src/app/area-chart/area-chart.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+@Component({
+  selector: 'app-area-chart',
+  templateUrl: './area-chart.component.html',
+  styleUrls: ['./area-chart.component.css']
+})
+export class AreaChartComponent {
+  Highcharts = Highcharts
+  chartOptions = {}
+  constructor(){
+    this.chartOptions = {
+      chart: {
+        renderTo: 'container',
+        type: 'column',
+        options3d: {
+            enabled: true,
+            alpha: 15,
+            beta: 15,
+            depth: 50,
+            viewDistance: 25
+        }
+    },
+    xAxis: {
+        categories: ['MEARN', 'PYTHON', 'DATA SCIENCE', 'TESTING', 'FLUTTER', '.NET',
+            'BIG DATA', 'JAVA SPRING', 'AI', 'CYBER SECURITY']
+    },
+    yAxis: {
+        title: {
+            enabled: false
+        }
+    },
+    tooltip: {
+        headerFormat: '<b>{point.key}</b><br>',
+        pointFormat: 'Placements: {point.y}'
+    },
+    title: {
+        text: 'Yearly Placement Analysis 2022 - 2023',
+        align: 'center'
+    },
+    subtitle: {
+        text: 'Luminar Technolab',
+        align: 'center'
+    },
+    legend: {
+        enabled: false
+    },
+    credits:{
+      enabled: false
+    },
+    plotOptions: {
+        column: {
+            depth: 25
+        }
+    },
+    series: [{
+        data: [1318, 1073, 1060, 813, 775, 745, 537, 444, 416, 395],
+        colorByPoint: true
+    }]
+    }
+    HC_exporting(Highcharts);
+  }
+  
+    
+}
